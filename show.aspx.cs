6fab922d5147d73251bb6a1566762c8741570bf5
Jiuhong Lv <heathen0507@gmail.com>
2020-05-02 09:43:52 +0800

    XiuGai2

--- show.aspx.cs
+++ show.aspx.cs
@@ -39,6 +39,47 @@ public partial class Show:System.Web.UI.Page
             SqlDataAdapter da2 = new SqlDataAdapter(selectSqlSpecial,sqlcon);
             DataSet ds =new DataSet();
             DataSet ds2 = new DataSet();
+            ds.Fill(ds,"studentInfo");
+            ds2.Fill(ds2,"studentInfo");
+            GridView1.DataSource =ds;
+            GridView2.DataSource = ds2;
+            GridView1.DataBind();
+            GridView2.DataBind();
+            signRows(ds,ds2);
+
+        }
+        else{
+            Response.Redirect("Showteacher.aspx");
+        }
+    }
+     private void signRows(DataSet ds, DataSet ds2)
+    {
+        for (int i = 0; i <= GridView1.Rows.Count - 1; i++)//遍历控件1
+        {
+            DataRowView drv = ds.Tables["studentInfo"].DefaultView[i];
+            string num = Convert.ToString(drv["stuNum"]);
+
+            for (int j = 0; j <= GridView2.Rows.Count - 1; j++)//遍历控件2
+            {
+                DataRowView drv2 = ds2.Tables["studentInfo"].DefaultView[j];
+                string num2 = Convert.ToString(drv2["stuNum"]);
+
+                bool b = false;
+                int result = string.Compare(num, num2);
+                if (result == 0) b = true;
+                if (b)
+                {
+                    GridView1.Rows[i].Cells[0].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[1].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[2].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[3].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[4].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[5].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[6].BackColor = System.Drawing.Color.Red;
+                    GridView1.Rows[i].Cells[7].BackColor = System.Drawing.Color.Red;
+                }
+            }
+
         }
     }
 }
